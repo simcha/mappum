68009460ddb1bb4d363fff496a70d8bc1f9e1c9d
Jan Topinski <simcha@chatka.org>
2009-11-04 08:25:41 +0100

    Add proper assertions and test for unidiretional mapping.

--- test/test_when.rb
+++ test/test_when.rb
@@ -11,13 +11,17 @@ class TestWhen < Test::Unit::TestCase
     foo = OpenStruct.new
     foo.age = 9
     foo.mem =11
+    foo.pol =15
     bar = rt.transform(foo, :foo)
     assert_equal(bar.tem, 11)
     assert_equal(bar.gem, nil)
+    assert_equal(bar.gol,15)
     foo.mem = 2
+    foo.pol = 0
     bar = rt.transform(foo, :foo)
     assert_equal(bar.gem, 2)
     assert_equal(bar.tem, nil)
+    assert_equal(bar.gol, nil)
    end
    def test_when_not
     catalogue = Mappum.catalogue
